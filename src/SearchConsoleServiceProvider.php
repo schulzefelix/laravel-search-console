c6fcd32c059590e14db40d7943b87e73144be25c
Felix <felix@schulze.co>
2017-08-07 13:29:43 +0200

    Implement Lumen Compatibility

--- src/SearchConsoleServiceProvider.php
+++ src/SearchConsoleServiceProvider.php
@@ -3,6 +3,8 @@
 namespace SchulzeFelix\SearchConsole;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
+use Illuminate\Foundation\Application as LaravelApplication;
 use SchulzeFelix\SearchConsole\Exceptions\InvalidConfiguration;
 
 class SearchConsoleServiceProvider extends ServiceProvider
@@ -14,9 +16,7 @@ class SearchConsoleServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/config/search-console.php' => config_path('search-console.php'),
-        ]);
+        $this->setupConfig();
     }
 
     /**
@@ -30,11 +30,11 @@ class SearchConsoleServiceProvider extends ServiceProvider
 
         $searchConsoleConfig = config('search-console');
 
-        $this->app->bind(SearchConsoleClient::class, function () use ($searchConsoleConfig) {
+        $this->app->singleton(SearchConsoleClient::class, function () use ($searchConsoleConfig) {
             return SearchConsoleClientFactory::createForConfig($searchConsoleConfig);
         });
 
-        $this->app->bind(SearchConsole::class, function () use ($searchConsoleConfig) {
+        $this->app->singleton(SearchConsole::class, function () use ($searchConsoleConfig) {
             $this->guardAgainstInvalidConfiguration($searchConsoleConfig);
 
             $client = app(SearchConsoleClient::class);
@@ -54,5 +54,18 @@ class SearchConsoleServiceProvider extends ServiceProvider
         if ($searchConsoleConfig['auth_type'] == 'oauth_json' && ! file_exists($searchConsoleConfig['connections']['oauth_json']['auth_config'])) {
             throw InvalidConfiguration::credentialsJsonDoesNotExist($searchConsoleConfig['connections']['oauth_json']['auth_config']);
         }
+    }
+
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__.'/config/search-console.php');
+
+        if ($this->app instanceof LaravelApplication) {
+            $this->publishes([$source => config_path('search-console.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('search-console');
+        }
+
+        $this->mergeConfigFrom($source, 'search-console');
     }
 }
